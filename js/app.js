eb190b4fb3dcbd59fc61b5d9c154de89cfd912fa
Franchette Camoro <nicolecamoro@gmail.com>
2017-01-13 15:16:01 +0800

    controllers

---
+++ js/app.js
@@ -0,0 +1,10 @@
+angular.module('todoApp', [])
+  .controller('TodoListController', ['$scope', function($scope) {
+
+    $scope.todos = [
+      {text:"Learn angular", done: true},
+      {text:"Give a talk", done: false}
+    ]
+
+
+  }]);
